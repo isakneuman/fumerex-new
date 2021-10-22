5578b0d363702b5858495ce7404c51c75b0661ff
isakneuman <63139694+isakneuman@users.noreply.github.com>
2021-10-22 15:32:44 +0600

    new

--- readsms.py
+++ readsms.py
@@ -1,33 +1,31 @@
 import gammu
 
-def readsms():
+def init_gsm():
     state_machine = gammu.StateMachine()
     state_machine.ReadConfig()
     state_machine.Init()
+    return state_machine
 
-    status = state_machine.GetSMSStatus()
-
-    remain = status["UnRead"]
-
+def get_new_message(state_machine):
+    status         =   state_machine.GetSMSStatus()
+    count_new_sms  =   status["UnRead"]
     start = True
-    
-    sms   = []
-
+    new_sms   = []
     try:
-        while remain > 0:
+        while count_new_sms > 0:
             if start:
                 sms = state_machine.GetNextSMS(Start=True, Folder=0)
                 start = False
             else:
                 sms = state_machine.GetNextSMS(Location=sms[0]["Location"], Folder=0)
-            remain = remain - len(sms)
+            count_new_sms = count_new_sms - len(sms)
 
             for m in sms:
                 if ( m["Text"].isdigit() ):
-                    print("{:<15}: {}".format("Text", m["Text"]))
+                    new_sms.append(m["Text"])
+            return new_sms
     except gammu.ERR_EMPTY:
-        # This error is raised when we've reached last entry
-        # It can happen when reported status does not match real counts
+            # This error is raised when we've reached last entry
+            # It can happen when reported status does not match real counts
         print("Failed to read all messages!")
 
-readsms()
